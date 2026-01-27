d56331e9c3285b047db6701a39a83a1a4973b0eb
Hspu1 <hspu1@tuta.io>
2026-01-27 23:07:20 +0300

    fix(auth): dynamic redirect_uri + workable UI under updates

--- app/api/auth/google_oauth2/router.py
+++ app/api/auth/google_oauth2/router.py
@@ -10,10 +10,10 @@ google_oauth2_router = APIRouter(tags=["google_oauth2"], prefix="/auth/google")
 
 @google_oauth2_router.get("/login")
 async def google_login(request: Request) -> Response:
-    # base_url = f"{request.url.scheme}://{request.url.netloc}"
-    # if "loca.lt" in base_url:
-    #     base_url = base_url.replace("http://", "https://")
-    # redirect_uri = f"{base_url}/auth/google/callback"
+    base_url = f"{request.url.scheme}://{request.url.netloc}"
+    if "loca.lt" in base_url:
+        base_url = base_url.replace("http://", "https://")
+    redirect_uri = f"{base_url}/auth/google/callback"
 
     google_url = await google_oauth.google.authorize_redirect(request, redirect_uri)
     url = google_url.headers.get("location")
