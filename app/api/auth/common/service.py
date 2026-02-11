6c6442b42fcdecf13281c464d1e260c18979f600
Hspu1 <hspu1@tuta.io>
2026-02-12 00:34:18 +0300

    fix(db): proper transactions impl, refine debug msgs

--- app/api/auth/common/service.py
+++ app/api/auth/common/service.py
@@ -11,7 +11,7 @@ from app.infra.postgres.models import UsersModel, UserIdentitiesModel
 
 async def get_user_id(user_info: dict, provider: AuthProvider, provider_user_id: str) -> str:
     start_time = time.perf_counter()
-    async with async_session_maker() as session:
+    async with async_session_maker.begin() as session:
         stmt = (
             select(UserIdentitiesModel.user_id)
             .where(
@@ -19,38 +19,36 @@ async def get_user_id(user_info: dict, provider: AuthProvider, provider_user_id:
                 UserIdentitiesModel.provider_user_id == provider_user_id
             )
         )
-
         if user_id := (await session.execute(stmt)).scalar():
+            print(f"[DB] READ total: {time.perf_counter() - start_time:.4f}s")
             return str(user_id)
 
-        async with session.begin():
-            verify_email = datetime.now(timezone.utc) \
-                if user_info.get("email_verified") else None
+        verify_email = datetime.now(timezone.utc) \
+            if user_info.get("email_verified") else None
 
-            stmt = insert(UsersModel).values(
-                email=user_info["email"],
-                name=user_info.get("name"),
-                email_verification_at=verify_email
+        stmt = insert(UsersModel).values(
+            email=user_info["email"],
+            name=user_info.get("name"),
+            email_verification_at=verify_email
+        )
+        user_upsert_stmt = stmt.on_conflict_do_update(
+            index_elements=['email'],
+            set_={'name': stmt.excluded.name},
+            where=or_(
+                user_info.get("email_verified", False),
+                UsersModel.email_verification_at.is_not(None)
             )
-            user_upsert_stmt = stmt.on_conflict_do_update(
-                index_elements=['email'],
-                set_={'name': stmt.excluded.name},
-                where=or_(
-                    user_info.get("email_verified", False),
-                    UsersModel.email_verification_at.is_not(None)
-                )
-            ).returning(UsersModel.id)
-            user_id = (await session.execute(user_upsert_stmt)).scalar_one()
+        ).returning(UsersModel.id)
+        user_id = (await session.execute(user_upsert_stmt)).scalar_one()
 
-            identity_upsert_stmt = (
-                insert(UserIdentitiesModel).values(
-                    user_id=user_id, provider=provider,
-                    provider_user_id=provider_user_id
-                ).on_conflict_do_nothing()
-            )
+        identity_upsert_stmt = (
+            insert(UserIdentitiesModel).values(
+                user_id=user_id, provider=provider,
+                provider_user_id=provider_user_id
+            ).on_conflict_do_nothing()
+        )
 
-            await session.execute(identity_upsert_stmt)
+        await session.execute(identity_upsert_stmt)
 
-        end_time = time.perf_counter()
-        print(f"[DATABASE] total: {(end_time - start_time):.4f}s")
-        return str(user_id)
+    print(f"[DB] WRITE/UPDATE total: {(time.perf_counter() - start_time):.4f}s")
+    return str(user_id)
