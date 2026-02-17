116be7872f97f02fbed8bd8919dbf80b877ee990
Hspu1 <hspu1@tuta.io>
2026-02-17 23:32:31 +0300

    refactor(infra)+chore: improve db logic + logging

--- app/api/auth/common/login.py
+++ app/api/auth/common/login.py
@@ -1,10 +1,15 @@
+import logging
 from secrets import token_urlsafe
+from time import perf_counter
 
 from fastapi import Request, Response
 from fastapi.responses import RedirectResponse
 from authlib.integrations.starlette_client.apps import StarletteOAuth2App
 
 from app.core.env_conf import auth_stg
+from app.utils import Colors
+
+logger = logging.getLogger(__name__)
 
 
 async def login(
@@ -12,6 +17,7 @@ async def login(
         provider: StarletteOAuth2App
 ) -> Response:
 
+    start = perf_counter()
     base_url = f"{request.url.scheme}://{request.url.netloc}"
     redirect_uri = f"{base_url}/auth/{provider_name}/callback"
 
@@ -30,6 +36,13 @@ async def login(
     else:
         provider_url = await provider.authorize_redirect(request, redirect_uri)
         url = provider_url.headers.get("location")
+
+    if logger.isEnabledFor(logging.DEBUG):
+        duration = (perf_counter() - start) * 1000
+        logger.debug(
+            "%s[AUTH LOGIN]%s provider=%s, total=%.2fms",
+            Colors.PURPLE, Colors.RESET, provider_name.upper(), duration
+        )
 
     if request.headers.get("HX-Request"):
         return Response(headers={"HX-Redirect": url})
--- app/api/auth/common/service.py
+++ app/api/auth/common/service.py
@@ -1,34 +1,37 @@
 import logging
-import time
+from time import perf_counter
 from datetime import datetime, timezone
 
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import or_, select
 
 from .schemas import AuthProvider
-from app.infra.postgres.service import pg_service
+from app.infra.postgres.service import PostgresService
 from app.infra.postgres.models import UsersModel, UserIdentitiesModel
 from app.utils import Colors
 
 logger = logging.getLogger(__name__)
 
 
-async def get_user_id(user_info: dict, provider: AuthProvider, provider_user_id: str) -> str:
-    start_time = time.perf_counter()
-    session_maker = pg_service.get_session_maker()
-    async with session_maker.begin() as session:
+async def get_user_id(
+        pg_svc: PostgresService, user_info: dict, provider: AuthProvider
+) -> str:
+
+    start_time = perf_counter()
+    session_maker = pg_svc.get_session_maker()
+    async with session_maker() as session:
         stmt = (
             select(UserIdentitiesModel.user_id)
             .where(
                 UserIdentitiesModel.provider == provider,
-                UserIdentitiesModel.provider_user_id == provider_user_id
+                UserIdentitiesModel.provider_user_id == user_info["id"]
             )
         )
 
         res = await session.execute(stmt)
         if user_id := res.scalar():
             if logger.isEnabledFor(logging.DEBUG):
-                dur_ms = (time.perf_counter() - start_time) * 1000
+                dur_ms = (perf_counter() - start_time) * 1000
                 logger.debug(
                     "%s[DB] READ%s user_id=%s...: total %s%.2fms%s",
                     Colors.PURPLE, Colors.RESET, str(user_id)[:8],
@@ -36,34 +39,37 @@ async def get_user_id(user_info: dict, provider: AuthProvider, provider_user_id:
                 )
             return str(user_id)
 
+    async with session_maker.begin() as session:
         verify_email = datetime.now(timezone.utc) \
-            if user_info.get("email_verified") else None
+            if user_info["email_verified"] else None
 
         stmt = insert(UsersModel).values(
-            email=user_info["email"],
-            name=user_info.get("name"),
+            email=user_info["email"], name=user_info["name"],
             email_verification_at=verify_email
         )
         user_upsert_stmt = stmt.on_conflict_do_update(
             index_elements=['email'],
-            set_={'name': stmt.excluded.name},
+            set_={
+                "name": stmt.excluded.name, "email": stmt.excluded.email,
+                "email_verification_at": stmt.excluded.email_verification_at
+            },
             where=or_(
-                bool(user_info.get("email_verified")),
+                bool(user_info["email_verified"]),
                 UsersModel.email_verification_at.is_not(None)
-            )
-        ).returning(UsersModel.id)
-        user_id = (await session.execute(user_upsert_stmt)).scalar_one()
+            ),
 
+        ).returning(UsersModel.id)
+
+        user_id = (await session.execute(user_upsert_stmt)).scalar_one()
         identity_upsert_stmt = (
             insert(UserIdentitiesModel).values(
                 user_id=user_id, provider=provider,
-                provider_user_id=provider_user_id
+                provider_user_id=user_info["id"]
             ).on_conflict_do_nothing()
         )
-
         await session.execute(identity_upsert_stmt)
 
-    dur_ms = (time.perf_counter() - start_time) * 1000
+    dur_ms = (perf_counter() - start_time) * 1000
     logger.debug(
         "%s[DB] WRITE/UPSERT%s user=%s: total %s%.2fms%s",
         Colors.PURPLE, Colors.RESET, user_info["email"],
