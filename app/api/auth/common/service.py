0ba01f80f121ad55f9f1414d27e661a594fda8ca
Hspu1 <hspu1@tuta.io>
2026-02-23 01:02:47 +0300

    refactor(infra): harden and optimize postgres impl (scheme + integration)

--- app/api/auth/common/service.py
+++ app/api/auth/common/service.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from datetime import datetime, timezone
 
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy import case, select
+from sqlalchemy import select
 
 from app.api.auth.common.schemas import AuthProvider
 from app.infra.postgres.service import PostgresService
@@ -10,60 +10,33 @@ from app.infra.postgres.models import UsersModel, UserIdentitiesModel
 from app.utils import log_debug_db
 
 
-async def get_user_id(
-        pg_svc: PostgresService, user_info: dict, provider: AuthProvider
-) -> str:
-    start_select = perf_counter()
-    session_maker = pg_svc.get_session_maker()
+async def get_user_id(pg_svc: PostgresService, user_info: dict, provider: AuthProvider) -> str:
+    start_time, session_maker = perf_counter(), pg_svc.get_session_maker()
     async with session_maker() as session:
-        stmt = (
-            select(UserIdentitiesModel.user_id)
-            .where(
-                UserIdentitiesModel.provider == provider,
-                UserIdentitiesModel.provider_user_id == user_info["id"]
-            )
+        stmt = select(UserIdentitiesModel.user_id).where(
+            UserIdentitiesModel.provider == provider.value,
+            UserIdentitiesModel.provider_user_id == user_info["id"]
         )
-
-        res = await session.execute(stmt)
-        if user_id := res.scalar():
-            log_debug_db(
-                op="READ", start_time=start_select,
-                detail=f"id={str(user_id)[:8]}.."
-            )
+        if user_id := (await session.execute(stmt)).scalar():
+            log_debug_db(op="READ", start_time=start_time, detail="id=%s..." % str(user_id)[:8])
             return str(user_id)
 
-    start_insert = perf_counter()
-    async with session.begin():
-        verify_email = datetime.now(timezone.utc) \
-            if user_info["email_verified"] else None
+    async with session_maker.begin() as session:
+        verify_at = datetime.now(timezone.utc) if user_info["email_verified"] else None
+        user_id = (await session.execute(
+            insert(UsersModel).values(
+                email=user_info["email"], name=user_info["name"],
+                email_verification_at=verify_at).on_conflict_do_update(index_elements=["email"],
+                    set_={"name": UsersModel.name}  # dummy update to force RETURNING id
+            ).returning(UsersModel.id)
+        )).scalar_one()
 
-        stmt = insert(UsersModel).values(
-            email=user_info["email"], name=user_info["name"],
-            email_verification_at=verify_email
-        )
-        user_upsert_stmt = stmt.on_conflict_do_update(
-            index_elements=["email"],
-            set_={
-                "name": stmt.excluded.name,
-                "email_verification_at": case(
-                    (user_info.get("email_verified") is True,
-                     stmt.excluded.email_verification_at),
-                    else_=UsersModel.email_verification_at
-                )
-            }
-        ).returning(UsersModel.id)
-
-        user_id = (await session.execute(user_upsert_stmt)).scalar_one()
-        identity_upsert_stmt = (
+        await session.execute(
             insert(UserIdentitiesModel).values(
-                user_id=user_id, provider=provider,
+                user_id=user_id, provider=provider.value,
                 provider_user_id=user_info["id"]
-            ).on_conflict_do_nothing()
+            ).on_conflict_do_nothing(index_elements=["provider", "provider_user_id"])
         )
-        await session.execute(identity_upsert_stmt)
 
-    log_debug_db(
-        op="UPSERT", start_time=start_insert,
-        detail=f'email: {user_info["email"]}'
-    )
+    log_debug_db(op="UPSERT", start_time=start_time, detail="email: %s" % user_info["email"])
     return str(user_id)
--- app/infra/postgres/models.py
+++ app/infra/postgres/models.py
@@ -3,7 +3,7 @@ from uuid import UUID
 
 from sqlalchemy import (
     String, ForeignKey, DateTime,
-    Boolean, Index
+    Boolean, Index, UniqueConstraint
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.infra.postgres.base import Base, TimestampMixin, UUIDv7Mixin
@@ -16,18 +16,23 @@ class UsersModel(Base, TimestampMixin, UUIDv7Mixin):
     email_verification_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None, server_default=None)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default="true")  # soft delete
-    identities: Mapped[list["UserIdentitiesModel"]] = relationship("UserIdentitiesModel", back_populates="user", cascade="all, delete-orphan")
+    identities: Mapped[list["UserIdentitiesModel"]] = relationship(
+        "UserIdentitiesModel", back_populates="user", cascade="all, delete-orphan"
+    )
 
     __table_args__ = (Index("idx_users_name", "name"),)
 
 
-class UserIdentitiesModel(Base):
+class UserIdentitiesModel(Base, TimestampMixin, UUIDv7Mixin):
     __tablename__ = "user_identities"
 
     user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    provider: Mapped[str] = mapped_column(String(50), nullable=False, primary_key=True)
-    provider_user_id: Mapped[str] = mapped_column(String(255), nullable=False, primary_key=True)
+    provider: Mapped[str] = mapped_column(String(50), nullable=False)
+    provider_user_id: Mapped[str] = mapped_column(String(255), nullable=False)
     password_hash: Mapped[str | None] = mapped_column(String(1024), nullable=True)
     user: Mapped["UsersModel"] = relationship("UsersModel", back_populates="identities")
 
-    __table_args__ = (Index('idx_identities_user_id', 'user_id'),)
+    __table_args__ = (
+        UniqueConstraint("provider", "provider_user_id", name="uq_provider_user"),
+        Index("idx_identities_user_id", "user_id"),
+    )
