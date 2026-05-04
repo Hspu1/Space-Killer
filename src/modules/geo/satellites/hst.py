14a213fe61ab82e6c8c390a7a8ccee676bf618e9
Hspu1 <hspu1@tuta.io>
2026-05-04 21:45:30 +0300

    chore(geo): delete unnecessary folder satellites

--- src/modules/geo/satellites/hst.py
+++
@@ -1,8 +0,0 @@
-from ..base_satellite import BaseSatellite
-from src.infra.nats.core_manager import CoreNATSManager
-
-
-class HST(BaseSatellite):
-    def __init__(self, nats: CoreNATSManager):
-        super().__init__(name="HST", nats=nats, interval=1.0)
-        self.norad_id = 20580
--- src/modules/geo/satellites/iss.py
+++
@@ -1,9 +0,0 @@
-from ..base_satellite import BaseSatellite
-
-from src.infra.nats.core_manager import CoreNATSManager
-
-
-class ISS(BaseSatellite):
-    def __init__(self, nats: CoreNATSManager):
-        super().__init__(name="ISS", nats=nats, interval=0.5)
-        self.norad_id = 25544
