58965f39091fab03e18ece178cb9a67b04000302
Hspu1 <hspu1@tuta.io>
2026-05-05 17:07:32 +0000

    fix(infra/redis): use separate DBs for specific tasks

--- src/core/env_conf.py
+++ src/core/env_conf.py
@@ -69,14 +69,14 @@ class RedisSettings(BaseSettings):
 
     redis_host: str = "redis"
     redis_port: int = 6379
-    redis_db: int = 0  # !!! - have to be fixed if needed
+    redis_sessions_db: int
+    redis_rl_db: int
     redis_password: str | None = None
 
     @cached_property
-    def db_url(self) -> str:
+    def rl_db_url(self) -> str:
         if self.redis_password:
-            return f"redis://:{self.redis_password}@{self.redis_host}:{self.redis_port}/{self.redis_db}"
-        return f"redis://{self.redis_host}:{self.redis_port}/{self.redis_db}"
+            return f"redis://:{self.redis_password}@{self.redis_host}:{self.redis_port}/{self.redis_rl_db}"
 
     # !!! 2 granian workers !!!, check limits
     general_max_connections: int = 200
--- src/infra/redis.py
+++ src/infra/redis.py
@@ -24,7 +24,7 @@ class RedisManager:
         self._client = Redis(
             host=self._config.redis_host,
             port=self._config.redis_port,
-            db=self._config.redis_db,
+            db=self._config.redis_sessions_db,
             password=self._config.redis_password,
             max_connections=self._config.general_max_connections,
             socket_connect_timeout=self._config.socket_connect_timeout,
@@ -39,7 +39,7 @@ class RedisManager:
             log_debug_redis(
                 op="CONNECTED",
                 start_time=start,
-                detail=f"{self._config.redis_host}:{self._config.redis_port}/db={self._config.redis_db}",
+                detail=f"{self._config.redis_host}:{self._config.redis_port}/db={self._config.redis_sessions_db}",
             )
 
         except (RedisConnError, TimeoutError, Exception) as e:
@@ -74,7 +74,7 @@ class RedisManager:
     def get_rate_limiter(self) -> RedisStore:
         if self._rate_limiter is None:
             self._rate_limiter = RedisStore(
-                server=self._config.db_url, options=self._get_options()
+                server=self._config.rl_db_url, options=self._get_options()
             )
         return self._rate_limiter
 
