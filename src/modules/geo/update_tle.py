836aecc7b32c053152bff0b8575393e15a619b02
Hspu1 <hspu1@tuta.io>
2026-05-10 18:14:16 +0000

    refactor(infra): use different groups for the upcoming microservices + rename redis_math to redis_geo

--- src/modules/geo/update_tle.py
+++ src/modules/geo/update_tle.py
@@ -44,7 +44,7 @@ headers = {
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 }
-
+# use curl-cffi instead nig
 
 
 def is_retryable(e: Exception) -> bool:
