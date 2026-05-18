8619d83b0630af9a100538600980f42632a137fc
Hspu1 <hspu1@tuta.io>
2026-05-18 18:39:26 +0300

    fix(models): fix imports among files

---
+++ src/infra/persistence/models/__init__.py
@@ -0,0 +1,6 @@
+from .profiles import ProfilesModel
+from .subscriptions import SubscriptionsModel
+from .user_identities import UserIdentitiesModel
+from .users import UsersModel
+
+__all__ = ["ProfilesModel", "SubscriptionsModel", "UserIdentitiesModel", "UsersModel"]
