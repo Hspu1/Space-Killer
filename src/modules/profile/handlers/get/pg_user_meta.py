3c0d46e25825fc74e337ba5bba4e44a384f47b31
Макс <hspu1@tuta.io>
2026-06-01 18:04:25 +0300

    feat(postgres): pg_resolve_user_meta impl (username + fid as user's meta info for the right panel in feed (any feed))

---
+++ src/modules/profile/handlers/get/pg_user_meta.py
@@ -0,0 +1,27 @@
+from time import perf_counter
+from typing import ReadOnly, TypedDict
+
+from sqlalchemy import select
+
+from src.infra.persistence.models.models import ProfilesModel
+from src.infra.persistence.postgres import PostgresManager
+from src.utils import log_debug_db
+
+
+class UserMeta(TypedDict):
+    username: ReadOnly[str]
+    avatar_fid: ReadOnly[str | None]
+
+
+async def pg_resolve_user_meta(user_id: str, pg_manager: PostgresManager) -> UserMeta:
+    start_time = perf_counter()
+    session_maker = pg_manager.get_session_maker()
+    async with session_maker() as session:
+        stmt = select(ProfilesModel.username, ProfilesModel.avatar_fid).where(
+            ProfilesModel.user_id == user_id
+        )
+        result = await session.execute(stmt)
+        row = result.tuple().one()
+
+        log_debug_db(op="READ", start_time=start_time, detail=f"user_id={user_id[:8]}...")
+        return {"username": row[0], "avatar_fid": row[1]}
